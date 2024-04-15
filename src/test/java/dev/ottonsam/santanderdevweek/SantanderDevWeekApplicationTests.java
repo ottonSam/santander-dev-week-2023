a6b2aa26da672ea36f28a83063a4ec73ee3f6fa0
Samuel Otton <ottonsam@gmail.com>
2024-04-15 13:31:16 -0300

    initial commit

---
+++ src/test/java/dev/ottonsam/santanderdevweek/SantanderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ottonsam.santanderdevweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
